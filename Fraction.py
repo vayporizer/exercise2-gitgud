0693fea16f0f32bf18bcd16c01d64408184a31a1
Paolo <paolo.gines@gmail.com>
2025-02-01 17:04:40 +0800

    Added exceptions for float inputs

--- Fraction.py
+++ Fraction.py
@@ -2,9 +2,10 @@ class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
         #TODO
-        # if int:
-
-        pass
+        if isinstance(numerator, float):
+            raise TypeError ("Numerator cannot be a decimal.")
+        if isinstance(denominator, float):
+            raise TypeError ("Denominator cannot be a float.")
 
     def gcd(a, b):
         #TODO
