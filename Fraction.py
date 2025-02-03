a347763d07edd56cda60f47851458c4da0840dd0
Paolo <paolo.gines@gmail.com>
2025-02-03 22:02:10 +0800

    Handling of float inputs with try except block

--- Fraction.py
+++ Fraction.py
@@ -2,21 +2,29 @@ class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
 
-        if isinstance(numerator, float):
-            raise TypeError ("Numerator cannot be a decimal.")
-        if isinstance(denominator, float):
-            raise TypeError ("Denominator cannot be a float.")
+        try:
+            if isinstance(numerator, float) or isinstance(denominator, float):
+                raise TypeError
+        except (TypeError):
+            self.numerator = 0
+            self.denominator = 1
+
+
+        # if isinstance(numerator, float):
+        #     raise TypeError ("Numerator cannot be a decimal.")
+        # if isinstance(denominator, float):
+        #     raise TypeError ("Denominator cannot be a float.")
         
-        if isinstance(numerator, str):
-            string_list = numerator.split("/", 1)
-            self.numerator = int(string_list[0])
-            self.denominator = int(string_list[1])
-        elif isinstance(numerator, int) and isinstance(denominator, int):
-            self.numerator = numerator
-            self.denominator = denominator
+        # if isinstance(numerator, str):
+        #     string_list = numerator.split("/", 1)
+        #     self.numerator = int(string_list[0])
+        #     self.denominator = int(string_list[1])
+        # elif isinstance(numerator, int) and isinstance(denominator, int):
+        #     self.numerator = numerator
+        #     self.denominator = denominator
         
-        if self.denominator == 0:
-            raise ZeroDivisionError ("Denominator cannot be zero.")
+        # if self.denominator == 0:
+        #     raise ZeroDivisionError ("Denominator cannot be zero.")
         
         if self.numerator < 0 and self.denominator < 0:
             self.numerator *= -1
